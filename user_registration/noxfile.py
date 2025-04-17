97b88d11438367f0f4fec60a3b507d9dc73762b3
wu <facundopareja@gmail.com>
2025-04-17 17:56:49 -0300

    chore:  nox setup

---
+++ user_registration/noxfile.py
@@ -0,0 +1,17 @@
+import shutil
+
+import nox
+
+# @nox.session()
+# def tests(session):
+#     session.install("pytest")
+#     session.run("pytest")
+
+@nox.session()
+def lint(session):
+    session.install("ruff")
+    session.run("ruff", "check")
+
+@nox.session
+def clean(session):
+    shutil.rmtree(".nox", ignore_errors=True)
