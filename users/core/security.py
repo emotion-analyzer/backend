a506b888ecba57497addcaa7ae0268e40f4d5546
wu <facundopareja@gmail.com>
2025-05-24 03:58:35 -0300

    refactor: adapted tests to new password reset flow

--- users/core/security.py
+++ users/core/security.py
@@ -17,20 +17,23 @@ def get_token(login: LoginUser, user: User,
     """Return a JWT token if given login data is valid."""
     if not verify_password(login.password, user, session):
         raise AuthError("Contraseña invalida.")
-    expiration_time = int(time.time()) + config.EXPIRATION_MINUTES_LOGIN * 60
     encoded_jwt = encode_token(
-        {"id": user.id, "email": login.email, "iss": "users",
-         "exp": expiration_time})
+        {"id": user.id, "email": login.email},
+        expiration_time=config.EXPIRATION_MINUTES_LOGIN * 60)
     return encoded_jwt
 
 def get_password_reset_token(email: EmailStr) -> str:
     """Return a JWT token for given email."""
-    expiration_time = int(time.time()) + config.EXPIRATION_MINUTES_PW_RESET * 60
-    return encode_token({"email": email, "iss": "users", "exp": expiration_time})
+    return encode_token({"email": email},
+                        expiration_time=config.EXPIRATION_MINUTES_PW_RESET * 60)
 
-def encode_token(data: dict[str, Any]) -> str:
+def encode_token(data: dict[str, Any], expiration_time: int) -> str:
     """Return token with encoded data."""
-    return jwt.encode( data, config.SECRET_KEY, algorithm=config.ALGORITHM,
+    data.update({
+        "iss": "users",
+        "exp": int(time.time()) + expiration_time
+    })
+    return jwt.encode(data, config.SECRET_KEY, algorithm=config.ALGORITHM,
         headers={"alg": config.ALGORITHM, "typ": "JWT", "kid": config.KONG_KEY})
 
 def decode_token(token):
