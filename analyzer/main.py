1c96c132da06b4e27a05fe3e67ba2314ea053fd2
wu <facundopareja@gmail.com>
2025-06-05 14:01:07 -0300

    chore: scraper linting

--- analyzer/main.py
+++ analyzer/main.py
@@ -1,6 +1,5 @@
 import asyncio
 from contextlib import asynccontextmanager
-from multiprocessing import Process
 
 from fastapi import FastAPI, Request
 from util.queue_middleware import process_posts
